917c79d749e9b44cabaa24ebb0949ff87dc0ce42
LaherMaciel <116970177+LaherMaciel@users.noreply.github.com>
2023-02-09 06:25:50 +0000

    ....  (-_-)

--- Push_swap/moviments.c
+++ Push_swap/moviments.c
@@ -18,7 +18,7 @@ void	ft_swap(t_list *stack, char id)
 {
 	t_list	*tmp;
 
-	if (stack == NULL || stack->next == NULL)
+	if (!stack || !stack->next)
 		return ;
 	if (id == 'a' || id == 'b')
 		ft_printf("s%c", id);
@@ -30,27 +30,27 @@ void	ft_swap(t_list *stack, char id)
 // ss -> sa and sb at the same time.
 void	ft_swap_swap(t_list *stack_a, t_list *stack_b)
 {
-	ft_swap(t_list stack_a, 'A');
-	ft_swap(t_list stack_b, 'B');
+	ft_swap(stack_a, 'A');
+	ft_swap(stack_b, 'B');
 	ft_printf("ss"); 
 }
 
-// Take the first element at the top of the stack_2 and put it at the top of the stack_1. 
-// Do nothing if stack_2 is empty.
+// Take the first element at the top of the stack_2 and put it at the
+// top of the stack_1. Do nothing if stack_2 is empty.
 // pa = stack b -> stack a ||  pb = stack a -> stack b
-void	ft_pass_to(t_list *stack_1, t_list *stack_2, char id)
+void	ft_pass_to(t_list **stack_1, t_list **stack_2, char id)
 {
 	t_list	*tmp;
 	int		val;
 
-	if (stack_2 == NULL)
+	if (!*stack_2 || !(*stack_2))
 		return ;
 	if (id == 'a' || id == 'b')
 		ft_printf("p%c", id);
-	tmp = stack_2;
-	stack_2 = stack_2->next;
-	tmp->next = stack_1;
-	stack_1 = tmp;
+	tmp = *stack_2;
+	*stack_2 = (*stack_2)->next;
+	tmp->next = *stack_1;
+	*stack_1 = tmp;
 }
 
 // Shift up all elements of the stack by 1. 
@@ -63,15 +63,15 @@ void	ft_rotate(t_list **stack, char id)
 		return ;
 	if (id == 'a' || id == 'b')
 		ft_printf("r%c", id);
-	new_last = stack;
-	stack = stack->next;
+	new_last = *stack;
+	*stack = (*stack)->next;
 	last = ft_lstlast(stack);
 	last->next = new_last;
-	new_last-> = NULL;
+	new_last->next = NULL;
 }
 
 // rr -> ra and rb at the same time
-void	ft_rotate_rotate(t_list stack_1, t_list stack_2)
+void	ft_rotate_rotate(t_list *stack_1, t_list *stack_2)
 {
 	ft_rotate(stack_1, 'A');
 	ft_rotate(stack_2, 'B');
