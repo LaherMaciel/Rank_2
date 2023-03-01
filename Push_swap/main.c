8d1a0cfebeebc966d31f6f43eb673fbdbed11561
LaherMaciel <Laherwpayotmaciel@gmail.com>
2023-03-01 19:25:40 +0000

    go

--- Push_swap/main.c
+++ Push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:43:29 by lwencesl          #+#    #+#             */
-/*   Updated: 2023/02/24 18:22:34 by lwencesl         ###   ########.fr       */
+/*   Updated: 2023/03/01 19:23:47 by lwencesl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ int	main(int argc, char *argv[])
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	int		done;
 
+	done = 0;
 	stack_a = store_integers_in_stack(argc, argv);
 	stack_b = NULL;
-	visualizer(&stack_a, &stack_b);
+	while (done == 0)
+	{
+		done = sort(&stack_a, &stack_b);
+		print_tab(stack_a, stack_a);
+	}
 	while (stack_a != NULL)
 		pop(&stack_a);
 	while (stack_b != NULL)
