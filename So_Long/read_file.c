8ca31193bab46a4156c26405926e200cd44a56ff
LaherMaciel <Laherwpayotmaciel@gmai.com>
2023-05-18 18:09:18 +0100

    .

--- So_Long/read_file.c
+++ So_Long/read_file.c
@@ -6,7 +6,7 @@
 /*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 19:13:45 by lwencesl          #+#    #+#             */
-/*   Updated: 2023/05/16 19:15:55 by lwencesl         ###   ########.fr       */
+/*   Updated: 2023/05/18 16:13:56 by lwencesl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,63 @@ int	read_file(void)
 	}
 	printf("\n\n");
 	return (0);
+}
+
+int	creat_map(void)
+{
+	int		fds;
+	char	*a;
+	char 	**map;
+	int		i;
+
+	i = 0;
+	a = "o";
+	fds = open("test.txt", O_RDONLY);
+	printf("\n");
+	while (a != NULL)
+	{
+		a = get_next_line(fds);
+		map[i++] = a;
+		free(a);
+	}
+	printf("\n\n");
+	return (0);
+}
+
+int	map_base_check(char *map[])
+{
+	int	x;
+	int	y;
+	int	x_max_len;
+	int	y_max_len;
+	int	player;
+	int	exit;		
+	int	collectible;
+
+	x = -1;
+	player = 0;
+	exit = 0;
+	collectible = 0;
+	//x_max_len = map;
+	while (x <= x_max_len)
+	{
+		y = -1;
+		while (y <= y_max_len)
+		{
+			if (x == 0 && y == 0 && map[x][y] != '1' || x == x_max_len && y == y_max_len && map[x][y] != '1')
+				return (0);
+			else if (x == 0 && map[x][y] != '1' || y == 0 && map[x][y] != '1')
+				return (0);
+			else if (x == x_max_len && map[x][y] != '1' || y == y_max_len && map[x][y] != '1')
+				return (0);
+			if (map[x][y] != 'p')
+				player++;
+			else if (map[x][y] != 'e')
+				exit++;
+			else if (map[x][y] != 'c')
+				collectible++;
+		}
+	}
+	if (player != 1 || exit != 1 || collectible < 1)
+		return (0);
 }
