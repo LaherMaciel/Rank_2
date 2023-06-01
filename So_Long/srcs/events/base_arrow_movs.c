8a9ad60269c481a7b15fcbfb7cbd3a3b218ffeb4
LaherMaciel <Laherwpayotmaciel@gmai.com>
2023-06-01 15:27:46 +0100

    .

--- So_Long/srcs/events/base_arrow_movs.c
+++ So_Long/srcs/events/base_arrow_movs.c
@@ -6,7 +6,7 @@
 /*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 14:08:25 by lwencesl          #+#    #+#             */
-/*   Updated: 2023/06/01 14:26:45 by lwencesl         ###   ########.fr       */
+/*   Updated: 2023/06/01 15:26:29 by lwencesl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	my_close(int keycode, t_win *win)
 	return (0);
 }
 
-int	mov_left(int keycode, t_win *win, t_map *map)
+int	mov_left(int keycode, t_map *map)
 {
 	int	i;
 	int	j;
@@ -43,7 +43,7 @@ int	mov_left(int keycode, t_win *win, t_map *map)
 	return (0);
 }
 
-int	mov_right(int keycode, t_win *win, t_map *map)
+int	mov_right(int keycode, t_map *map)
 {
 	int	i;
 	int	j;
@@ -68,12 +68,59 @@ int	mov_right(int keycode, t_win *win, t_map *map)
 	return (0);
 }
 
-int	mov_up(int keycode, t_win *win, t_map *map)
+int	mov_up(int keycode, t_map *map)
 {
+	int	i;
+	int	j;
+
+	j = -1;
+	while (mpa->mapa[0][++j])
+	{
+		if (map->mapa[0][j] == 'p')
+			return (0);
+	}
+	i = -1;
+	while (map->mapa[++i])
+	{
+		j = -1;
+		while (map->mapa[i][++j])
+		{
+			if (map->mapa[i][j] == 'p')
+			{
+				map->mapa[i - 1][j] = map->mapa[i][j];
+				map->mapa[i][j] = '0';
+			}
+		}
+	}
 	return (0);
 }
 
-int	mov_down(int keycode, t_win *win, t_map *map)
+int	mov_down(int keycode, t_map *map)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map->mapa[i])
+		i++:
+	j = -1;
+	while (mpa->mapa[i][++j])
+	{
+		if (map->mapa[i][j] == 'p')
+			return (0);
+	}
+	i = -1;
+	while (map->mapa[++i])
+	{
+		j = -1;
+		while (map->mapa[i][++j])
+		{
+			if (map->mapa[i][j] == 'p')
+			{
+				map->mapa[i + 1][j] = map->mapa[i][j];
+				map->mapa[i][j] = '0';
+			}
+		}
+	}
 	return (0);
 }
